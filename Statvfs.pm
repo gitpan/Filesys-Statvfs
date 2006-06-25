830b60c6c72f5643b523c60618487ac84cf1de1f
Ian Guthrie <IGuthrie@aol.com>
2006-06-25 05:59:54 +0000

    Import of IGUTHRIE/Filesys-Statvfs-0.82 from CPAN.
    gitpan-cpan-distribution: Filesys-Statvfs
    gitpan-cpan-version:      0.82
    gitpan-cpan-path:         IGUTHRIE/Filesys-Statvfs-0.82.tar.gz
    gitpan-cpan-author:       IGUTHRIE
    gitpan-cpan-maturity:     released

--- Statvfs.pm
+++ Statvfs.pm
@@ -8,7 +8,7 @@ require DynaLoader;
 
 @ISA = qw(Exporter DynaLoader);
 @EXPORT = qw(statvfs fstatvfs);
-$VERSION = '0.80';
+$VERSION = '0.82';
 bootstrap Filesys::Statvfs $VERSION;
 
 1;
@@ -39,15 +39,20 @@ Interface for statvfs() and fstatvfs()
 
 Unless you need access to the bsize, flag, and namemax values,
 you should probably look at using Filesys::DfPortable or
-Filesys::Df instead.
+Filesys::Df instead. They will generally provide you with more
+functionality and portability.
 
-The statvfs() and fstatvfs() functions will return a list of
-values, or will return undef and set $! if there was an error.
+The module should work with all flavors of Unix that implement the
+C<statvfs()> and C<fstatvfs()> calls. This would include Linux, *BSD,
+HP-UX, AIX, Solaris, Mac OS X, Irix, Cygwin, etc ...
 
-The values returned are described in the statvfs header or
-the statvfs() man page.
+The C<statvfs()> and C<fstatvfs()> functions will return a list of
+values, or will return C<undef> and set C<$!> if there was an error.
 
-The module assumes that if you have statvfs(), fstatvfs() will
+The values returned are described in the statvfs/fstatvfs header or
+the C<statvfs()/fstatvfs()> man page.
+
+The module assumes that if you have C<statvfs()>, C<fstatvfs()> will
 also be available.
 
 =head1 AUTHOR
@@ -61,6 +66,6 @@ Copyright (c) 2006 Ian Guthrie. All rights reserved.
 
 =head1 SEE ALSO
 
-statvfs(2), Fileys::DfPortable, Filesys::Df, Filesys::Statfs
+statvfs(2), fstatvfs(2), Filesys::DfPortable, Filesys::Df
 
 =cut
